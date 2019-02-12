879e48c8523c595be19181ee6bdfe4333f2ee16a
fwcno1 <190404614@qq.com>
2019-02-12 14:25:23 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,16 @@ import Router from 'vue-router'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 Vue.use(MintUI)
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      // path: '/',
+      // name: 'HelloWorld',
+      // component: HelloWorld
     }
   ]
 })
